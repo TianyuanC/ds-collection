08218160d7b041eabebadd264288d838defbdd90
Ryan Chu <tianyuan.chu@gmail.com>
2019-04-29 23:46:57 -0700

    test cases

--- problem_1.py
+++ problem_1.py
@@ -1,22 +1,35 @@
+class Node:
+    def __init__(self, key, val, pre=None, nxt=None):
+        self.val = val
+        self.key = key
+        self.pre = pre
+        self.nxt = nxt
+
+
 class LRU_Cache(object):
 
     def __init__(self, capacity):
-        # Initialize class variables
-        pass
+        self.head = None
+        self.tail = None
+        self.lookup = {}
+        self.capacity = capacity
 
     def get(self, key):
-        # Retrieve item from provided key. Return -1 if nonexistent.
         pass
 
     def set(self, key, value):
-        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        pass
+
+    def display_cache(self):
         pass
 
 
-our_cache = LRU_Cache(1)
+cache = LRU_Cache(2)
 
-our_cache.set(1, 1)
-our_cache.set(2, 2)
-our_cache.get(1)  # returns -1
-our_cache.get(2)  # returns 2
-our_cache.get(3)  # returns -1
+cache.set(1, 1)
+cache.set(2, 2)
+assert cache.get(2) == 2
+assert cache.get(1) == 1
+cache.set(3, 10)
+assert cache.get(2) == -1
+assert cache.get(3) == 10
