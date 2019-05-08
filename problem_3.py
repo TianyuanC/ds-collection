6cf7b3984ddbb30fe330f246db837b2b070af02c
Ryan Chu <tianyuan.chu@gmail.com>
2019-05-08 01:17:14 -0700

    huffman implementation

--- problem_3.py
+++ problem_3.py
@@ -1,17 +1,77 @@
 import sys
+from heapq import *
+from collections import defaultdict
+
+
+class Node:
+    def __init__(self, left=None, right=None, value=None):
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def __lt__(self, other):
+        pass
+
+
+class Code:
+    def __init__(self, tree):
+        self._codes = {}
+        self.tree = tree
+        self._generate_code("", self.tree)
+
+    def _generate_code(self, pre, node):
+        if node.value != None:
+            self._codes[node.value] = pre
+        else:
+            self._generate_code(pre+"0", node.left)
+            self._generate_code(pre+"1", node.right)
+
+    def encode(self, txt):
+        encoded = ""
+        for char in txt:
+            encoded += self._codes[char]
+        return encoded
 
 
 def huffman_encoding(data):
-    pass
+    frequency = defaultdict(int)
+    for char in data:
+        frequency[char] += 1
+    heap = []
+    for key, value in frequency.items():
+        node = Node(None, None, key)
+        heappush(heap, (value, node))
+
+    while len(heap) > 1:
+        freq_1, node_1 = heappop(heap)
+        freq_2, node_2 = heappop(heap)
+        root = Node(node_1, node_2)
+        heappush(heap, (freq_1 + freq_2, root))
+
+    freq, root = heappop(heap)
+    codes = Code(root)
+
+    return codes.encode(data), root
 
 
 def huffman_decoding(data, tree):
-    pass
+    decoded = ""
+    node = tree
+    for bit in data:
+        if bit == "0":
+            node = node.left
+        else:
+            node = node.right
+        if node.value is None:
+            continue
+        else:
+            decoded += node.value
+            node = tree
+
+    return decoded
 
 
 if __name__ == "__main__":
-    codes = {}
-
     a_great_sentence = "The bird is the word"
 
     print("The size of the data is: {}\n".format(
@@ -19,7 +79,6 @@ if __name__ == "__main__":
     print("The content of the data is: {}\n".format(a_great_sentence))
 
     encoded_data, tree = huffman_encoding(a_great_sentence)
-
     print("The size of the encoded data is: {}\n".format(
         sys.getsizeof(int(encoded_data, base=2))))
     print("The content of the encoded data is: {}\n".format(encoded_data))
